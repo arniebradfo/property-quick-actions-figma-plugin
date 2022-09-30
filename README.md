1cbfb489df69019523246ba164ec9ca030dae8a3
James Bradford <j.bradford@pnnl.gov>
2022-09-30 14:25:56 -0700

    trying out figma menu parameters with autoLayoutSpacingBetweenItems

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Add Object Property Settings to the Quick Actions toolbar for fewer clicks 
 
 (Under Development)
+
+## TODO
+- type manifest by importing to a types file?
+- toggle boolean options, with optional parameters?
+- test parameters
--- code.ts
+++ code.ts
@@ -1,41 +1,61 @@
-figma.on('run', (event) => {
+figma.on('run', (event: RunEvent) => {
 	console.log(figma.currentPage.selection[0]);
 
 	const command = event.command as keyof typeof commandMap;
-	figma.currentPage.selection.forEach((node) => commandMap[command](node));
+	const parameters = event.parameters as any; //
+
+	figma.currentPage.selection.forEach((node) => commandMap[command](node, parameters));
 	figma.closePlugin();
+});
+
+figma.parameters.on('input', ({ parameters, key, query, result }: ParameterInputEvent) => {
+	console.log({ parameters, key, query, result });
+	const options = optionsMap[key];
+	if (options == null) return; // assume "allowFreeform": true
+	const suggestions = typeof options === 'function' ? options(query) : searchSuggestions(query, options);
+	result.setSuggestions(suggestions);
 });
 
 // Auto Layout /////////////////////
 
-const autoLayoutDirectionToggle: Cmd = (node) => {
+const autoLayoutDirectionToggle: Command = (node) => {
 	if ('layoutMode' in node) {
 		if (node.layoutMode === 'VERTICAL') node.layoutMode = 'HORIZONTAL';
 		else node.layoutMode = 'VERTICAL';
 	}
 };
-const autoLayoutSpacingModeSpaceBetween: Cmd = (node) => {
+
+// abstract setNumericValue(node: SceneNode, numericProperty:string, value:string)
+const autoLayoutSpacingBetweenItems: Command<{ pixels: string }> = (node, parameters) => {
+	if ('itemSpacing' in node) {
+		const pixels = parseFloat(parameters.pixels);
+		if (!isNaN(pixels)) node.itemSpacing = pixels;
+		else figma.notify('Error');
+	}
+};
+
+const autoLayoutSpacingModeSpaceBetween: Command = (node) => {
 	if ('primaryAxisAlignItems' in node) node.primaryAxisAlignItems = 'SPACE_BETWEEN';
 };
-const autoLayoutSpacingModePacked: Cmd = (node) => {
+const autoLayoutSpacingModePacked: Command = (node) => {
 	if ('primaryAxisAlignItems' in node) {
 		if (node.primaryAxisAlignItems !== 'SPACE_BETWEEN') return;
 		node.primaryAxisAlignItems = 'MIN';
 	}
 };
-const autoLayoutStrokesExcluded: Cmd = (node) => {
+const autoLayoutStrokesExcluded: Command = (node) => {
 	if ('strokesIncludedInLayout' in node) node.strokesIncludedInLayout = false;
 };
-const autoLayoutStrokesIncluded: Cmd = (node) => {
+const autoLayoutStrokesIncluded: Command = (node) => {
 	if ('strokesIncludedInLayout' in node) node.strokesIncludedInLayout = true;
 };
-const autoLayoutCanvasStackingLast: Cmd = (node) => {
+const autoLayoutCanvasStackingLast: Command = (node) => {
 	if ('itemReverseZIndex' in node) node.itemReverseZIndex = false;
 };
-const autoLayoutCanvasStackingFirst: Cmd = (node) => {
+const autoLayoutCanvasStackingFirst: Command = (node) => {
 	if ('itemReverseZIndex' in node) node.itemReverseZIndex = true;
 };
-const autoLayoutTextBaselineAlignmentToggle: Cmd = (node) => {
+const autoLayoutTextBaselineAlignmentToggle: Command = (node) => {
 	if ('layoutMode' in node && node.layoutMode === 'HORIZONTAL') {
 		if (node.counterAxisAlignItems !== 'BASELINE') {
 			node.counterAxisAlignItems = 'BASELINE';
@@ -51,10 +71,14 @@ const autoLayoutTextBaselineAlignmentToggle: Cmd = (node) => {
 
 // Command Map /////////////////////
 
-type Cmd = (node: SceneNode) => void;
+type Command<ParametersType = Record<string, string> | undefined> = (
+	node: SceneNode,
+	parameters: RunEvent<ParametersType>['parameters']
+) => void;
 
 const commandMap = {
 	autoLayoutDirectionToggle,
+	autoLayoutSpacingBetweenItems,
 	autoLayoutSpacingModeSpaceBetween,
 	autoLayoutSpacingModePacked,
 	autoLayoutStrokesExcluded,
@@ -62,4 +86,28 @@ const commandMap = {
 	autoLayoutCanvasStackingLast,
 	autoLayoutCanvasStackingFirst,
 	autoLayoutTextBaselineAlignmentToggle,
+};
+
+type GetSuggestions = (query?: string, options?: string[]) => string[];
+const searchSuggestions: GetSuggestions = (query = '', options = []) => {
+	// TODO: fuzzy find
+	return options.filter((s) => s.includes(query));
+};
+
+type CreateSuggestions = (query?: string) => string[];
+
+const validateInt: CreateSuggestions = (query = '') => {
+	const int = parseInt(query);
+	if (isNaN(int)) return [];
+	else return [int.toString()];
+};
+const validateFloat: CreateSuggestions = (query = '') => {
+	const float = parseFloat(query);
+	if (isNaN(float)) return [];
+	else return [float.toString()];
+};
+
+const optionsMap: Record<string, string[] | CreateSuggestions | undefined> = {
+	pixels: validateFloat,
+	example: ['put', 'suggestions', 'here'],
 };
