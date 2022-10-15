f197c4d03a8313cec93efd4948d317820486f2d9
James Bradford <j.bradford@pnnl.gov>
2022-10-14 18:22:27 -0700

    update project config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 // const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const path = require('path');
-const webpack = require('webpack');
+// const webpack = require('webpack');
 
 module.exports = (env, argv) => ({
 	mode: argv.mode === 'production' ? 'production' : 'development',
